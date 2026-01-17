ad39f747896cf911251f86b64ef6c301c198fc5f
Huy <Huyanh052222@gmail.com>
2026-01-17 21:16:05 +0700

    L

--- main.js
+++ main.js
@@ -101,11 +101,11 @@ const solutions = {
         });
     },
 // ----------------------------------------------------------------------------------------------------------------------------  
-    3: () => {
+    3: () =>{
         log('Solution for Cau 4 executed.');
         let RawData = inputField.value;
 
-        if (!RawData) {
+        if(!RawData) {
             log('No input provided.');
             log('Please check your fill data (test case)!');
             return;
@@ -148,7 +148,7 @@ const solutions = {
 
     },
 // ----------------------------------------------------------------------------------------------------------------------------  
-    5: () => {
+    5: () =>{
         log('Solution for Cau 6 executed.');
         log('Taking IP...');
 
@@ -159,7 +159,7 @@ const solutions = {
             }
             return response.json();
         })
-        .then(data => {
+        .then(data =>{
             log('-----------------------------');
             log(`My IP Address: ${data.ip}`);
             log('-----------------------------');
@@ -170,12 +170,12 @@ const solutions = {
     }
 };
 
-buttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
+buttons.forEach((button, index) =>{
+    button.addEventListener('click', () =>{
         clearConsole();
-        if (solutions[index]) {
+        if(solutions[index]) {
             solutions[index]();
-        } else {
+        } else{
             log('No solution defined for this button.');
             
         }
@@ -184,14 +184,14 @@ buttons.forEach((button, index) => {
 
 
 //Cau 1
-function forEchTest(array, callback) {
+function forEchTest(array, callback){
     for (let i = 0; i < array.length; i++) {
         callback(array[i], i, array);
     }
 }
 
 //Cau 2
-function forEachPair(array, callback) {
+function forEachPair(array, callback){
     for(let i = 0; i < array.length - 1; i++) {
         let currentElement = array[i];
         let nextElement = array[i + 1];
@@ -201,49 +201,49 @@ function forEachPair(array, callback) {
 
 
 //cau 5
-function fetchAPIData() {
+function fetchAPIData(){
     const screen = document.querySelector('.screen-cau5');
     const container = document.getElementById('countries-container');
     const closeBtn = document.getElementById('close');
 
-    if (!screen || !container) {
+    if(!screen || !container){
         log('Error!');
         return;
     }
 
     screen.style.display = "flex";
 
-    closeBtn.onclick = function() {
+    closeBtn.onclick = function(){
         screen.style.display = "none";
     };
 
-    async function loadAllCountries() {
-        try {
-            if (container.children.length > 1) return; 
+    async function loadAllCountries(){
+        try{
+            if(container.children.length > 1) return; 
 
             container.innerHTML = '<div class="message">Loading data...</div>';
             
             const response = await fetch('https://open.oapi.vn/location/countries');
             
-            if (!response.ok) throw new Error('Error API connection');
+            if(!response.ok) throw new Error('Error API connection');
 
             const data = await response.json();
             
             console.log("Loaded data:", data);
 
             let list = [];
-            if (Array.isArray(data)) list = data;
-            else if (data && Array.isArray(data.data)) list = data.data;
-            else if (typeof data === 'object') list = Object.values(data);
+            if(Array.isArray(data)) list = data;
+            else if(data && Array.isArray(data.data)) list = data.data;
+            else if(typeof data === 'object') list = Object.values(data);
 
-            if (list.length === 0) {
+            if(list.length === 0) {
                 container.innerHTML = '<div class="message">No found any contry data</div>';
                 return;
             }
 
             let htmlContent = ''; 
 
-            list.forEach(country => {                
+            list.forEach(country =>{                
                 const name = country.name?.common || country.name || country.countryName || "Unknown Name";
 
                 
@@ -252,7 +252,8 @@ function fetchAPIData() {
                 else if (country.flags?.png) flag = country.flags.png;
                 else if (country.flag) flag = country.flag;
 
-                htmlContent += `
+                htmlContent += 
+                `
                     <div class="card">
                         <div class="flag-container">
                             <img src="${flag}" loading="lazy" onerror="this.src='https://via.placeholder.com/300x160?text=Image+Error'">
@@ -270,7 +271,7 @@ function fetchAPIData() {
 
             container.innerHTML = htmlContent;
 
-        } catch (error) {
+        }catch (error) {
             console.error(error);
             container.innerHTML = `<div class="message error">Error: ${error.message}</div>`;
         }
